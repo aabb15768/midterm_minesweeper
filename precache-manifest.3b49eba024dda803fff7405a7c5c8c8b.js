39f2f0241f2dc7f832e2a4939c52d47cc3593a3c
aabb15768 <b03204032@ntu.edu.tw>
2019-05-21 20:25:03 +0800

    Updates

---
+++ precache-manifest.3b49eba024dda803fff7405a7c5c8c8b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b082057d527e7b27134",
+    "url": "/midterm_minesweeper/static/css/main.0458f7ab.chunk.css"
+  },
+  {
+    "revision": "0b082057d527e7b27134",
+    "url": "/midterm_minesweeper/static/js/main.a0be021b.chunk.js"
+  },
+  {
+    "revision": "2b1135ba21612722ea96",
+    "url": "/midterm_minesweeper/static/js/runtime~main.ff91e134.js"
+  },
+  {
+    "revision": "a6885df189f9f002285c",
+    "url": "/midterm_minesweeper/static/js/2.59390ab1.chunk.js"
+  },
+  {
+    "revision": "1517918573e289534a9006f8fb9be6ad",
+    "url": "/midterm_minesweeper/static/media/flag.15179185.png"
+  },
+  {
+    "revision": "2d4abfe08e4c9b19a658317f762edd06",
+    "url": "/midterm_minesweeper/static/media/easy.2d4abfe0.png"
+  },
+  {
+    "revision": "6f511e4c71e01b72fced59ad183e759f",
+    "url": "/midterm_minesweeper/static/media/medium.6f511e4c.png"
+  },
+  {
+    "revision": "756d2d336ea28cbf6a76246dde04c089",
+    "url": "/midterm_minesweeper/static/media/hard.756d2d33.png"
+  },
+  {
+    "revision": "0006461ae714073d229291f960b74b30",
+    "url": "/midterm_minesweeper/static/media/mine-menu-icon.0006461a.png"
+  },
+  {
+    "revision": "821525386b0319d0bbe53ed70b8bed9a",
+    "url": "/midterm_minesweeper/static/media/window-controls.82152538.png"
+  },
+  {
+    "revision": "582cac299faac1ab8d378a6488ba2ced",
+    "url": "/midterm_minesweeper/static/media/digital-dismay.582cac29.otf"
+  },
+  {
+    "revision": "65cb344c64e3d29ae35396789de64c2e",
+    "url": "/midterm_minesweeper/static/media/prstart.65cb344c.ttf"
+  },
+  {
+    "revision": "e3a9c893cd405aca729270d3cbc49559",
+    "url": "/midterm_minesweeper/static/media/micross.e3a9c893.ttf"
+  },
+  {
+    "revision": "11df47dbdf20ecbfeaba3022ec2b5511",
+    "url": "/midterm_minesweeper/static/media/start.11df47db.png"
+  },
+  {
+    "revision": "336132b5ff25ce39c4979d33be4b6a1e",
+    "url": "/midterm_minesweeper/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/midterm_minesweeper/precache-manifest.3b49eba024dda803fff7405a7c5c8c8b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/midterm_minesweeper/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
